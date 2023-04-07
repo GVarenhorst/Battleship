84c1a8e36fee915856b2320b188f4e221ed64c81
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-07 09:39:40 -0500

    Add test to render cell

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,53 +2,81 @@ require 'spec_helper'
 
 RSpec.describe Cell do
   before(:each) do
-    @cell = Cell.new("B4")
+    @cell_1 = Cell.new("B4")
+    @cell_2 = Cell.new("C3")
     @cruiser = Ship.new("Cruiser", 3)
   end
 
   describe '#initialize' do
     it 'exists' do
 
-      expect(@cell).to be_a(Cell)
+      expect(@cell_1).to be_a(Cell)
     end
 
     it 'has a coordinate' do
 
-      expect(@cell.coordinate).to eq("B4")
+      expect(@cell_1.coordinate).to eq("B4")
     end
 
     it 'it can contain a ship' do
 
-      expect(@cell.ship).to eq(nil)
+      expect(@cell_1.ship).to eq(nil)
     end
 
     it 'cell is empty' do
 
-      expect(@cell.empty?).to eq(true)
+      expect(@cell_1.empty?).to eq(true)
     end
   end
 
   describe '#placing ship' do
     it 'places ship in a cell' do
 
-      @cell.place_ship(@cruiser)
+      @cell_1.place_ship(@cruiser)
 
-      expect(@cell.ship).to eq(@cruiser)
-      expect(@cell.empty?).to eq(false)
+      expect(@cell_1.ship).to eq(@cruiser)
+      expect(@cell_1.empty?).to eq(false)
     end
   end
 
-  describe 'fired upon' do
+  describe '#fired upon' do
     it 'registers a hit on a ship' do
 
-      @cell.place_ship(@cruiser)
+      @cell_1.place_ship(@cruiser)
 
-      expect(@cell.fired_upon?).to eq(false)
+      expect(@cell_1.fired_upon?).to eq(false)
 
-      @cell.fire_upon
+      @cell_1.fire_upon
 
-      expect(@cell.ship.health).to eq(2)
-      expect(@cell.fired_upon).to eq(true)
+      expect(@cell_1.ship.health).to eq(2)
+      expect(@cell_1.fired_upon).to eq(true)
+    end
+  end
+
+  describe '#render cell' do
+    it 'returns a string representation of a cell' do
+      
+      expect(@cell_1.render).to eq(".")
+      
+      @cell_1.fire_upon
+      
+      expect(@cell_1.render).to eq("M")
+
+      @cell_2.place_ship(@cruiser)
+
+      expect(@cell_2.render).to eq(".")
+      expect(@cell_2.render(true)).to eq("S")
+
+      @cell_2.fire_upon
+
+      expect(@cell_2.render).to eq("H")
+      expect(@cruiser.sunk?).to eq(false)
+
+      @cruiser.hit
+      @cruiser.hit
+
+      expect(@cruiser.sunk?).to eq(true)
+      expect(@cell_2.render).to eq("X")
     end
   end
 end
