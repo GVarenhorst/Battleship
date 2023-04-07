ed5fb800f4b4553fcc5078bf6b3a422cc79e42be
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-07 17:13:52 -0500

    Add render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,5 +1,5 @@
 class Cell
-  attr_reader :coordinate, :ship, :fired_upon
+  attr_reader :coordinate, :ship, :fired_upon, :render
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
@@ -32,6 +32,21 @@ class Cell
       @fired_upon = true
     else
       @fired_upon = true
+    end
+  end
+
+  def render(view = false)
+    if view == false && @fired_upon == false  
+      "."
+    elsif view == true && @fired_upon == false
+      "S"
+    elsif @fired_upon == true && @ship == nil
+      "M"
+    elsif @ship.sunk? 
+      "X"
+    else   
+      @ship != nil && @fired_upon == true
+      "H"
     end
   end
 end
