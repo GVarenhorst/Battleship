7628d7ebed26b667ec8dc396b9c1b4a0a7916144
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-11 16:29:27 -0500

    Add initialize and start game menu test and pass

---
+++ spec/game_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe Game do
+  before(:each) do
+    @game = Game.new
+  end
+  describe '#initialize' do
+    it 'exists' do
+
+      expect(@game).to be_a(Game)
+    end
+  end
+  describe '#start game' do
+    it 'has a start menu' do
+      expect(@game.menu).to eq("Welcome to BATTLESHIP Enter p to play. Enter q to quit")
+    end
+  end
+end
