3161b7f974c0fe1f84b8b54fd6fcca2b999f9ee8
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-12 08:13:25 -0500

    Modify computer places ships test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,8 +11,15 @@ RSpec.describe Game do
     end
   end
   describe '#start game' do
-    it 'has a start menu' do
+    xit 'has a start menu' do
       expect(@game.menu).to eq("Welcome to BATTLESHIP Enter p to play. Enter q to quit")
+    end
+  end
+
+  describe 'computer places ships' do 
+    it 'can place ships on computer board' do
+
+      expect(@game.place_comp_submarine.valid_placement?).to eq(true)
     end
   end
 end
