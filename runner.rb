31cf7b92dcdb0a78b3ce85f8adc9b3332c35740f
Javen Wilson <javenb022@gmail.com>
2023-04-12 23:00:56 -0400

    Add code to test methods

--- runner.rb
+++ runner.rb
@@ -2,7 +2,11 @@ require './spec/spec_helper'
 
 game1 = Game.new
 
-game1.place_human_submarine
-game1.place_human_cruiser
+# game1.place_human_submarine
+# game1.place_human_cruiser
+# game1.display_boards
 
-
+puts "Time for me to take my shot!"
+human_keys = @game1.human_board.cells.keys
+target = human_keys.sample(1)
+human_keys.delete(target[0])
