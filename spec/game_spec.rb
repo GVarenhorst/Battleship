a7c277da320c0c04fd334e0be06976b6c0e057af
Javen Wilson <javenb022@gmail.com>
2023-04-13 13:57:33 -0400

    Remove pseudocode

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,23 +4,17 @@ RSpec.describe Game do
   before(:each) do
     @game = Game.new
   end
+  
   describe '#initialize' do
     it 'exists' do
 
       expect(@game).to be_a(Game)
     end
   end
+
   describe '#start game' do
-    it 'has a start menu' do
+    xit 'has a start menu' do
       expect(@game.menu).to eq("Welcome to BATTLESHIP Enter p to play. Enter q to quit")
     end
   end
-#   describe 'computer places ships' do 
-#     xit 'can place ships on computer board' do
-
-#       expect(@game.place_comp_cruiser).to eq(true)
-#       expect(@game.place_comp_submarine).to eq(true)
-#       #These two tests, when they fail, our 'expected' does not match 'got', yet we see a correct return value every time with multiple testing.
-#     end
-#   end
 end
