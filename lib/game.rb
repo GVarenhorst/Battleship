c75cdc30985c88c2a815fd9a3ab273228c2e4825
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-12 20:18:19 -0500

    Add place_comp_cruiser/submarine methods, place_human_cruiser/submarine methods, and invalid_placement_cruiser/submarine methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,4 @@
-require 'board'
+require './lib/board'
 class Game
   attr_reader :menu
   def initialize
@@ -7,6 +7,7 @@ class Game
     @human_submarine = Ship.new("submarine", 2)
     @human_cruiser = Ship.new("cruiser", 3)
     @comp_board = Board.new
+    @human_board = Board.new
   end
   
 
@@ -29,21 +30,81 @@ class Game
   #   end
   # end  
 
+  # def place_comp_submarine
+  #   cells_keys = @comp_board.cells.keys.sample(2)
+  #   until @comp_board.place(@comp_submarine, cells_keys) do  
+  #     place_comp_submarine
+  #   end
   def place_comp_submarine
     cells_keys = @comp_board.cells.keys.sample(2)
-    until @comp_board.place(@comp_submarine, cells_keys) do  
+    if @comp_board.valid_placement?(@comp_submarine, cells_keys)
+      @comp_board.place(@comp_submarine, cells_keys)
+    else
       place_comp_submarine
-
     end
-    # randomly select coordinates from @cells hash and then call comp_submarine.place && comp_cruiser.place method
-    
-
-    
+    cells_keys
   end
+
+  def place_comp_cruiser
+    cells_keys = @comp_board.cells.keys.sample(3)
+    if @comp_board.valid_placement?(@comp_cruiser, cells_keys)
+      @comp_board.place(@comp_cruiser, cells_keys)
+    else
+      place_comp_cruiser
+    end
+  end
+
+  def place_human_submarine
+    puts "I have placed my ships accordingly."
+    puts "Time for you to place yours."
+    puts "Choose 2 coordinates for submarine: "
+    sub_coords = gets.chomp.upcase.split
+    if @human_board.valid_placement?(@human_submarine, sub_coords)
+      @human_board.place(@human_submarine, sub_coords)
+    else
+      invalid_placement_sub
+    end
+  end
+  
+  def place_human_cruiser
+    puts "I have placed my ships accordingly."
+    puts "Time for you to place yours."
+    puts "Choose 3 coordinates for cruiser: "
+    cruiser_coords = gets.chomp.upcase.split()
+    if @human_board.valid_placement?(@human_cruiser, cruiser_coords)
+      @human_board.place(@human_cruiser, cruiser_coords)
+    else
+      invalid_placement_cruiser
+    end
+  end
+
+  def invalid_placement_sub
+    puts "Sorry, those are invalid coordinates. Please Try Again: "
+    sub_coords = gets.chomp.upcase.split()
+    if @human_board.valid_placement?(@human_submarine, sub_coords)
+      @human_board.place(@human_submarine, sub_coords)
+    else
+      invalid_placement_sub
+    end
+  end
+
+  def invalid_placement_cruiser
+    puts "Sorry, those are invalid coordinates. Please Try Again: "
+    cruiser_coords = gets.chomp.upcase.split()
+    if @human_board.valid_placement?(@human_cruiser, cruiser_coords)
+      @human_board.place(@human_cruiser, cruiser_coords)
+    else
+      invalid_placement_cruiser
+    end
+  end
+  
+
+
+  #if human coordinates are invalid p "Those coordinates are invalid. Please try again." then loop back to .place_human_cruiser/submarine
 end
 
 #Trying to decide if menu interaction belongs
- #in menu def or whether menu should be called 
+#in menu def or whether menu should be called 
   #in runner.rb file.
 
 #
